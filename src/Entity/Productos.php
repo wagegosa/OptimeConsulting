e791f7cdfdeb036fa7e1c3e743449abf6aa35463
Gerardo González <wagegosa@gmail.com>
2020-01-09 17:21:29 -0500

    Migración
    Se realiza migración de la DB

---
+++ src/Entity/Productos.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductosRepository")
+ */
+class Productos
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=120)
+     */
+    private $codigo;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $descripcion;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $marca;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $categoria;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCodigo(): ?string
+    {
+        return $this->codigo;
+    }
+
+    public function setCodigo(string $codigo): self
+    {
+        $this->codigo = $codigo;
+
+        return $this;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(string $descripcion): self
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    public function getMarca(): ?string
+    {
+        return $this->marca;
+    }
+
+    public function setMarca(string $marca): self
+    {
+        $this->marca = $marca;
+
+        return $this;
+    }
+
+    public function getCategoria(): ?float
+    {
+        return $this->categoria;
+    }
+
+    public function setCategoria(float $categoria): self
+    {
+        $this->categoria = $categoria;
+
+        return $this;
+    }
+}
